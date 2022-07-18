432fc3e49e360c1d68dd9dd1ffcf3043b986cb91
Axel Lorreyne <Axel.Lorreyne@UGent.be>
2022-07-18 18:35:55 +0200

    Add app tiles + cleaned up mock data

--- js/index.js
+++ js/index.js
@@ -5,20 +5,24 @@ import 'font-awesome/css/font-awesome.css'
 window.onload = async () => {
     const apps = [
         {
-            name: 'App 1',
-            description: 'App 1'
+            name: 'KNoodle',
+            description: 'KNoodle is KNoWS\' Solid-based alternative to Doodle.',
+            link: 'https://github.com/KNowledgeOnWebScale/knoodle/'
         },
         {
-            name: 'App 2',
-            description: 'App 2'
+            name: 'SolidEditor',
+            description: 'A Microsoft Monaco based editor for text files on a Solid pod',
+            link: 'https://github.com/phochste/SolidEditor'
         },
         {
-            name: 'App 2',
-            description: 'App 2'
+            name: 'AcmeUpload',
+            description: 'A Solid app that can be used as a dropzone for a container.',
+            link: 'https://github.com/phochste/AcmeUpload'
         },
         {
-            name: 'App 2',
-            description: 'App 2'
+            name: 'AcmeContainer',
+            description: 'A base implementation of a Solid App with login and loading a Container',
+            link: 'https://github.com/phochste/AcmeContainer'
         },
     ]
     const categories = [
@@ -34,9 +38,35 @@ window.onload = async () => {
 async function makeAppTile(app) {
     const $applist = document.getElementById('app-list');
     const $div = document.createElement('div');
-    const $line = document.createElement('p');
-    $line.innerText = app.name;
-    $div.appendChild($line);
+    $div.setAttribute('class', 'card mx-3 my-2');
+    $div.setAttribute('style', 'width: 13rem');
+
+    const $link = document.createElement('a');
+    $link.setAttribute('href', app.link);
+    $link.setAttribute('target', '_blank');
+
+    const $img = document.createElement('img');
+    $img.setAttribute('class', 'card-img-top p-5');
+    $img.setAttribute('style', 'cursor: pointer');
+    $img.setAttribute('src', 'https://genr.eu/wp/wp-content/uploads/2018/10/logo.svg');
+    $link.appendChild($img)
+
+    $div.appendChild($link);
+
+    const $body = document.createElement('div');
+    $body.setAttribute('class', 'card-body');
+
+    const $title = document.createElement('h5');
+    $title.setAttribute('class', 'card-title');
+    $title.innerText = app.name;
+    $body.appendChild($title);
+
+    const $text = document.createElement('p');
+    $text.setAttribute('class', 'card-text');
+    $text.innerText =app.description;
+    $body.appendChild($text);
+
+    $div.appendChild($body);
     $applist.appendChild($div);
 }
 
