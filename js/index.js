87f893e8f741a6c830e10cd7dc23f806785fd334
Axel Lorreyne <Axel.Lorreyne@UGent.be>
2022-08-03 11:34:17 +0200

    add loading icon during query

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ async function handleNewApp(app) {
  * Handle various document elements when app query has ended
  */
 function handleAppQueryFinished() {
-    // TODO: create loading icon and hide it here
+    document.getElementById('loader').setAttribute('class', 'loader hidden');
     if (apps.length === 0) {
         document.getElementById('no-results-title').setAttribute('class', '');
     }
